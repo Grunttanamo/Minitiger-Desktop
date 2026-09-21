ed799973849e5a2acba314ca0089fb0eaa72cd46
Grunttanamo <53061205+Grunttanamo@users.noreply.github.com>
2026-09-21 06:52:07 +0200

    Improve run script when executable has not been built

--- dev/windows/run.bat
+++ dev/windows/run.bat
@@ -1,13 +1,24 @@
 @echo off
-REM Jellyfin Desktop - Run built executable
+REM Minitiger Desktop - Run built executable
 REM Run build.bat first
 
 setlocal
 call "%~dp0common.bat"
 call "%~dp0common.bat" :setup_runtime || exit /b 1
 
+set "APP_EXE=%BUILD_DIR%\src\%EXE_NAME%"
+
+if not exist "%APP_EXE%" (
+    echo ERROR: Minitiger Desktop executable was not found:
+    echo   %APP_EXE%
+    echo.
+    echo Run this first:
+    echo   dev\windows\build.bat
+    exit /b 1
+)
+
 REM === Run ===
-"%BUILD_DIR%\src\%EXE_NAME%" %*
+"%APP_EXE%" %*
 set EXIT_CODE=%ERRORLEVEL%
 
 endlocal & exit /b %EXIT_CODE%
