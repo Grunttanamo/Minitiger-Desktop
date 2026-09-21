bf2bfd724ceafedb947251f50f0a258d9720c66d
Grunttanamo <53061205+Grunttanamo@users.noreply.github.com>
2026-09-21 06:48:14 +0200

    Phase 1.1: add experimental embedded VLC video surface

---
+++ src/player/VlcVideoItem.h
@@ -0,0 +1,56 @@
+#ifndef VLCVIDEOITEM_H
+#define VLCVIDEOITEM_H
+
+#include <QImage>
+#include <QMutex>
+#include <QQuickPaintedItem>
+#include <QString>
+
+#include <vlc/libvlc.h>
+
+class QPainter;
+
+class VlcVideoItem : public QQuickPaintedItem
+{
+    Q_OBJECT
+
+public:
+    explicit VlcVideoItem(QQuickItem* parent = nullptr);
+    ~VlcVideoItem() override;
+
+    void paint(QPainter* painter) override;
+
+    Q_INVOKABLE bool playSource(const QString& source);
+    Q_INVOKABLE void pausePlayback();
+    Q_INVOKABLE void resumePlayback();
+    Q_INVOKABLE void stopPlayback();
+
+    QString lastError() const { return m_lastError; }
+
+private:
+    static void* lockVideo(void* opaque, void** planes);
+    static void unlockVideo(void* opaque, void* picture, void* const* planes);
+    static void displayVideo(void* opaque, void* picture);
+    static unsigned setupVideoFormat(void** opaque,
+                                     char* chroma,
+                                     unsigned* width,
+                                     unsigned* height,
+                                     unsigned* pitches,
+                                     unsigned* lines);
+    static void cleanupVideoFormat(void* opaque);
+
+    bool ensureVlc();
+    void releasePlayer();
+    void setError(const QString& error);
+
+    libvlc_instance_t* m_vlc = nullptr;
+    libvlc_media_player_t* m_mediaPlayer = nullptr;
+
+    QImage m_frame;
+    QMutex m_frameMutex;
+    QString m_lastError;
+    unsigned m_videoWidth = 0;
+    unsigned m_videoHeight = 0;
+};
+
+#endif // VLCVIDEOITEM_H
